20bc5df921d2df76f2b6e5b46ab4ca90a8ebdefe
ChristianKirschberg <christian@kvalifik.dk>
2023-02-27 20:36:00 +0100

    initial

--- script.js
+++ script.js
@@ -6,71 +6,16 @@ const scissorsButton = document.getElementById('scissors');
 const result = document.getElementById('result');
 
 // Add event listeners to the buttons
-rockButton.addEventListener('click', () => playRound('rock'));
-paperButton.addEventListener('click', () => playRound('paper'));
-scissorsButton.addEventListener('click', () => playRound('scissors'));
-
-
 
 
 // Define the playRound function
 function playRound(playerSelection) {
     // Generate a random choice for the computer
-    const choices = ['rock', 'paper', 'scissors'];
-    const computerSelection = choices[Math.floor(Math.random() * choices.length)];
-  
-    // Determine the winner based on the choices
-    let winner;
-    if (playerSelection === computerSelection) {
-      winner = 'tie';
-    } else if (
-      (playerSelection === 'rock' && computerSelection === 'scissors') ||
-      (playerSelection === 'paper' && computerSelection === 'rock') ||
-      (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-      winner = 'player';
-    } else {
-      winner = 'computer';
-    }
-  
-    // Display the result on the screen
-    if (winner === 'tie') {
-      result.textContent = "It's a tie!";
-    } else if (winner === 'player') {
-      result.textContent = "You win! " + playerSelection + " beats " + computerSelection + ".";
-
-      emailjs.sendForm('service_gzdackn', 'template_q0sinj9', document.getElementById('game'))
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    } else {
-      result.textContent = "You lose! " + computerSelection + " beats " + playerSelection + ".";
-    }
+    //Math.random()
+    
+    
+    
+    
+      result.textContent = "Who won?";
+    
   }
-
-
-  const freetext = document.getElementById('inputFreetext')
-  const question = document.getElementById('inputQuestion')
-
-  freetext.addEventListener("click", () => {
-    document.getElementById('divFreetext').style.display = 'block'
-    document.getElementById('divQuestion').style.display = 'none'
-  });
-  question.addEventListener("click", () => {
-    document.getElementById('divQuestion').style.display = 'block'
-    document.getElementById('divFreetext').style.display = 'none'
-  });
-
-
-  document.getElementById('formFreetext').addEventListener('submit', (e) => {
-        e.preventDefault();
-        
-        emailjs.sendForm('service_gzdackn', 'template_ulynbvb', document.getElementById('formFreetext'))
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-  });
--- trafficlight.js
+++ trafficlight.js
@@ -6,16 +6,11 @@ function switchLights() {
 
     // Write if else if statements, to go from red being active, to yellow being active, to green
     // and back to red.
-    if (red.classList.contains("active")) {
-        red.classList.remove("active");
-        yellow.classList.add("active");
-    } else if (yellow.classList.contains("active")) {
-        yellow.classList.remove("active")
-        green.classList.add("active")
-    } else if (green.classList.contains("active")) {
-        green.classList.remove("active")
-        red.classList.add("active")
-    }
+    
+    // The following will evaluate to true or false
+    red.classList.contains("active")
+
+
 }
 
 document.getElementById('change-light').addEventListener('click', switchLights);
