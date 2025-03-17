4b71a8e690a30903779a786ca6b0fbee1b5b624e
Christian Kirschberg <ckirschberg@gmail.com>
2025-03-17 10:36:43 +0100

    løsning

--- trafficlight.js
+++ trafficlight.js
@@ -1,17 +1,29 @@
 function switchLights() {
 
-    const red = document.getElementById('red')
-    const yellow = document.getElementById('yellow')
-    const green = document.getElementById('green')
+    // udvælger jeg elementer i html-siden til senere brug
+    const red = document.getElementById('red');
+    const yellow = document.getElementById('yellow');
+    const green = document.getElementById('green');
 
     // Write if else if statements, to go from red being active, to yellow being active, to green
     // and back to red.
     
     // The following will evaluate to true or false
-    red.classList.contains("active")
-
-
+                    // er der en css-klasse 'active' på id="red"
+    const isActiveRed = red.classList.contains('active');
+    console.log(isActiveRed); // test, er rød aktiv
+    if(isActiveRed) { // Er rød aktiv?
+        red.classList.remove('active'); // fjern active css-klassen på elementet 'rød'
+        yellow.classList.add('active'); // tilføj active css-klassen på elementet 'gul'
+    } else if (yellow.classList.contains('active')) { // Er yellow active?
+        yellow.classList.remove('active');
+        green.classList.add('active');
+    } else { // Hvis red og yellow IKKE er active, så..
+        green.classList.remove('active'); // gør det her
+        red.classList.add('active') // rød bliver tændt igen
+    }
 }
 
+// Tilknyt event-listener på at brugeren trykker på knappen
 document.getElementById('change-light').addEventListener('click', switchLights);
 
