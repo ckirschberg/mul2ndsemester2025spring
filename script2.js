4bf56c5ba840998924242327cc6f40ea4e3b490a
Christian Kirschberg <ckirschberg@gmail.com>
2025-03-19 10:21:49 +0100

    getFullName

--- script2.js
+++ script2.js
@@ -13,7 +13,29 @@ function loadPersonData() {
     // indsæt data i html elementerne.
     // udvælge html elementerne ud fra deres id
     // innerHTML = 'noget'
+    document.getElementById('firstname').innerHTML = person1.firstname;
+    document.getElementById('lastname').innerHTML = person1.lastname;
+    document.getElementById('email').innerHTML = person1.email;
 
 }
 
 document.getElementById("loadPerson").addEventListener('click', loadPersonData)
+
+// number er nu en parameter dvs. input til funktionen.
+function giveMeANumber(number) {
+    alert(number);
+}
+giveMeANumber(15);
+
+function getName() {
+    const name = "Emily"
+    return name;
+}
+
+const name = getName()
+// alert("Dit navn er " + name)
+
+function getFullName(/** send person objektet ind som parameter */) {
+
+    return //...
+}
